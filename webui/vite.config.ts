154bb45e2584e78a69ac69e82610f9af55010bec
liangyin233 <liangyingugugu@163.com>
2026-03-12 20:11:56 +0800

    chore(webui): update vite config

--- webui/vite.config.ts
+++ webui/vite.config.ts
@@ -4,10 +4,11 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: "0.0.0.0",
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:18792',
+        target: 'http://0.0.0.0:18792',
         changeOrigin: true
       }
     }
